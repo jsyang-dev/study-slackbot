15beed51820ed66287d28664679e4c426a026140
Jeongsu Yang <mycat83@gmail.com>
2023-08-26 20:57:05 +0900

    Initial commit

---
+++ src/main/java/me/study/slackbot/StudySlackbotApplication.java
@@ -0,0 +1,13 @@
+package me.study.slackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySlackbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySlackbotApplication.class, args);
+    }
+
+}
