744f448993d5c59b83c3a693c2e89922d0735fc0
Jeongsu Yang <mycat83@gmail.com>
2023-08-27 22:28:02 +0900

    feat: /hello command 처리 추가

--- src/main/java/me/study/slackbot/SlackApp.java
+++ src/main/java/me/study/slackbot/SlackApp.java
@@ -1,5 +1,6 @@
 package me.study.slackbot;
 
+import com.slack.api.app_backend.slash_commands.payload.SlashCommandPayload;
 import com.slack.api.bolt.App;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.model.event.AppMentionEvent;
@@ -12,6 +13,13 @@ public class SlackApp {
     public App initSlackApp() {
         App app = new App();
 
+        appMentionEvent(app);
+        helloCommand(app);
+
+        return app;
+    }
+
+    private static void appMentionEvent(App app) {
         app.event(AppMentionEvent.class, (payload, ctx) -> {
             final AppMentionEvent event = payload.getEvent();
             final MethodsClient client = ctx.client();
@@ -22,7 +30,12 @@ public class SlackApp {
 
             return ctx.ack();
         });
+    }
 
-        return app;
+    private static void helloCommand(App app) {
+        app.command("/hello", (req, ctx) -> {
+            final SlashCommandPayload payload = req.getPayload();
+            return ctx.ack(String.format("<@%s> What's up?", payload.getUserName()));
+        });
     }
 }
